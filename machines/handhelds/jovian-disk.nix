a4143d5b8a61c5f8f491e6353f1ac86815e9cc8d
Trey Fordham <treyf711@gmail.com>
2024-11-16 12:44:49 -0500

    Update the steam deck disk configuration and update the flake lock.

--- machines/handhelds/jovian-disk.nix
+++ machines/handhelds/jovian-disk.nix
@@ -2,15 +2,17 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/vda";
         type = "disk";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              type = "EF00";
-              size = "1024M";
               priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "1024M";
+              type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -20,11 +22,33 @@
             };
             root = {
               size = "100%";
-              priority = 2;
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "/nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                  # Subvolume for the swapfile
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "4G";
+                    };
+                  };
+                };
               };
             };
           };
@@ -33,3 +57,4 @@
     };
   };
 }
+
