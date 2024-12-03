5eb65ded77b32cfcfeac9a44815798747657be67
Trey Fordham <treyf711@gmail.com>
2024-12-03 08:36:02 -0500

    maybe the final update

--- roles/server/selfhosted-services.nix
+++ roles/server/selfhosted-services.nix
@@ -1,12 +1,34 @@
-{config, pkg, lib, ...}:
+{config, pkgs, lib, ...}:
 {
-  services.jellyfin.enable = true;
-  services.radarr.enable = true;
-  services.sonarr.enable = true;
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+             "plexmediaserver"
+           ];
 
+  services.jellyfin.enable = true;
+  services.jellyfin.openFirewall = true;
+  services.plex.enable = true;
+  services.plex.openFirewall = true;
+  services.plex.group = "media";
+  services.radarr.enable = true;
+  services.radarr.group = "media";
+  services.sonarr.enable = true;
+  services.sonarr.group = "media";
+  
   virtualisation.oci-containers = {
       backend = "podman";
       containers = {
+        qbittorrent = {
+	  image = "lscr.io/linuxserver/qbittorrent:latest";
+	  autoStart = true;
+	  ports = [ "8080:8080" "6881:6881" "6881:6881/udp" ];
+	  volumes = [ "qbittorrent-config:/config" ];
+	  environment = {
+	    PGID = "991";
+	    TZ = "Etc/UTC";
+	    WEBUI_PORT = "8080";
+	    TORRENTING_PORT = "6881";
+	  };
+	};
         flare-solvarr = {
           image = "ghcr.io/flaresolverr/flaresolverr:latest";
           autoStart = true;
@@ -27,6 +49,17 @@
             TZ="America/New_York";
           };
         };
+	nzbget = {
+	  image = "lscr.io/linuxserver/nzbget:latest";
+	  autoStart = true;
+	  volumes = [ "nzbget-config:/config" "/storage/downloads:/downloads" ];
+	  ports = [ "6789:6789" ];
+	  environment = {
+	    PUID = "1000";
+	    PGID = "991";
+	    TZ = "Etc/UTC";
+	  };
+	};
       };
     };
 
